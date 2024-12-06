dec14278d4d51a34e0cd0406b2f6713780739f7f
vividograu <00001119842815sp@al.educacao.sp.gov.br>
2024-12-06 15:01:51 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Alura Destroyer
+// @namespace    https://cursos.alura.com.br/
+// @version      2024-05-12
+// @description  eu to cansado jefe é mt coisakkkk
+// @author       marcos10pc
+// @match        https://cursos.alura.com.br/course/*/task/*
+// @icon         https://i.imgur.com/gP1LZq9.png
+// @grant        none
+// @require      https://raw.githubusercontent.com/marcos10pc/Alura-Destroyer/refs/heads/main/hawk_tuah.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("https://pixgg.com/marcos10pc"); 
+})();
